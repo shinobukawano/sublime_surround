85854f7c797253c72eed0487664e47f36ec81715
Shinobu Kawano <kawanoshinobu@gmail.com>
2012-08-10 23:32:47 +0900

    My first sublime text customize plugin :)

---
+++ sublime_surround.py
@@ -0,0 +1,28 @@
+import sublime_plugin
+
+
+class SublimeSurroundCommand(sublime_plugin.TextCommand):
+
+    def on_done(self, word):
+
+        for region in self.view.sel():
+
+            if not region.empty():
+
+                # Get the selected text
+                s = self.view.substr(region)
+
+                # Surround word by input word
+                surroundStr = word + s + word
+
+                # Replace the selection with transformed text
+                self.view.replace(self.edit, region, surroundStr)
+
+
+    def run(self, edit):
+
+        self.edit = edit
+
+        # Show input panel for surround word, then handling input
+        self.view.window().show_input_panel('Surrond Word is', '',
+            self.on_done, None, None)
